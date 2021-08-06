330912c94868c6f48c89f6b5d736704f3c0f1325
ShadowCodesYT <88476590+ShadowCodesYT@users.noreply.github.com>
2021-08-06 09:53:04 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,140 @@
+print('[INFO] Importing MODULES')
+import discord
+import json
+from discord.ext import commands
+import datetime
+import time
+import random
+import asyncio
+from discord.errors import Forbidden
+
+time.sleep(0.5) 
+
+
+print('[INFO] Loading DATA')
+with open("config.json") as file: # Load the config file
+    info = json.load(file)
+    token = info["token"]
+    prefix = info["prefix"]
+
+time.sleep(0.5) 
+
+
+print('[INFO] LOADING BOT')
+client = commands.Bot(command_prefix = prefix,intents=discord.Intents.all())
+client.remove_command("help")
+
+time.sleep(0.5) 
+print('[INFO] LOADING EVENTS')
+
+@client.event
+async def on_ready():
+    print("[Connected]")
+    activity = discord.Game(name="discord.gg/devscafe", type=3)
+    await client.change_presence(status=discord.Status.online, activity=activity)
+
+
+
+@client.command()
+@commands.has_permissions(manage_channels=True)
+async def giveaway(ctx):
+    # Giveaway command requires the user to have a "Giveaway Host" role to function properly
+
+    # Stores the questions that the bot will ask the user to answer in the channel that the command was made
+    # Stores the answers for those questions in a different list
+    giveaway_questions = ['Which channel will I host the giveaway in?', 'What is the prize?', 'How long should the giveaway run for (in seconds)?',]
+    giveaway_answers = []
+
+    # Checking to be sure the author is the one who answered and in which channel
+    def check(m):
+        return m.author == ctx.author and m.channel == ctx.channel
+    
+    # Askes the questions from the giveaway_questions list 1 by 1
+    # Times out if the host doesn't answer within 30 seconds
+    for question in giveaway_questions:
+        await ctx.send(question)
+        try:
+            message = await client.wait_for('message', timeout= 30.0, check= check)
+        except asyncio.TimeoutError:
+            await ctx.send('You didn\'t answer in time.  Please try again and be sure to send your answer within 30 seconds of the question.')
+            return
+        else:
+            giveaway_answers.append(message.content)
+
+    # Grabbing the channel id from the giveaway_questions list and formatting is properly
+    # Displays an exception message if the host fails to mention the channel correctly
+    try:
+        c_id = int(giveaway_answers[0][2:-1])
+    except:
+        await ctx.send(f'You failed to mention the channel correctly.  Please do it like this: {ctx.channel.mention}')
+        return
+    
+    # Storing the variables needed to run the rest of the commands
+    channel = client.get_channel(c_id)
+    prize = str(giveaway_answers[1])
+    time = int(giveaway_answers[2])
+
+    # Sends a message to let the host know that the giveaway was started properly
+    await ctx.send(f'The giveaway for {prize} will begin shortly.\nPlease direct your attention to {channel.mention}, this giveaway will end in {time} seconds.')
+
+    # Giveaway embed message
+    give = discord.Embed(color = 0x2ecc71)
+    give.set_author(name = f'GIVEAWAY TIME!', icon_url = 'https://i.imgur.com/VaX0pfM.png')
+    give.add_field(name= f' {prize}!', value = f'React with 🎉 to enter!\n Ends in {round(time/60, 2)} minutes!', inline = False)
+    end = datetime.datetime.utcnow() + datetime.timedelta(seconds = time)
+    give.set_footer(text = f'Hosted By {ctx.author.name}')
+    my_message = await channel.send(embed = give)
+    
+    # Reacts to the message
+    await my_message.add_reaction("🎉")
+    await asyncio.sleep(time)
+
+    new_message = await channel.fetch_message(my_message.id)
+
+    # Picks a winner
+    users = await new_message.reactions[0].users().flatten()
+    users.pop(users.index(client.user))
+    winner = random.choice(users)
+
+    # Announces the winner
+    winning_announcement = discord.Embed(color = 0xff2424)
+    winning_announcement.set_author(name = f'THE GIVEAWAY HAS ENDED!', icon_url= 'https://i.imgur.com/DDric14.png')
+    winning_announcement.add_field(name = f'🎉 Prize: {prize}', value = f'🥳 **Winner**: {winner.mention}\n 🎫 **Number of Entrants**: {len(users)}', inline = False)
+    winning_announcement.set_footer(text = 'Thanks for entering!')
+    await channel.send(embed = winning_announcement)
+
+
+
+@client.command()
+@commands.has_permissions(manage_channels=True)
+async def reroll(ctx, channel: discord.TextChannel, id_ : int):
+    # Reroll command requires the user to have a "Giveaway Host" role to function properly
+    try:
+        new_message = await channel.fetch_message(id_)
+    except:
+        await ctx.send("Incorrect id.")
+        return
+    
+    # Picks a new winner
+    users = await new_message.reactions[0].users().flatten()
+    users.pop(users.index(client.user))
+    winner = random.choice(users)
+
+    # Announces the new winner to the server
+    reroll_announcement = discord.Embed(color = 0xff2424)
+    reroll_announcement.set_author(name = f'The giveaway was re-rolled by the host!', icon_url = 'https://i.imgur.com/DDric14.png')
+    reroll_announcement.add_field(name = f'🥳 New Winner:', value = f'{winner.mention}', inline = False)
+    await channel.send(embed = reroll_announcement)
+
+@client.event
+async def on_command_error(ctx, error):
+  await ctx.send(f'{error}')
+
+
+time.sleep(0.5) 
+
+print('[INFO] CONNECTING TO API')
+
+
+client.run(token)
+
